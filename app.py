147e41fb7ec892da9c87496d2b852082726c26dc
Chase Farmer <chasefarmer2808@ufl.edu>
2018-05-28 19:26:25 -0400

    on and off buttons

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import RPi.GPIO as GPIO
 
 from flask import Flask, render_template
 
-GPIO.setmode(GPIO.BOARD)
 app = Flask(__name__)
 
 R = 33
@@ -11,7 +10,9 @@ B = 29
 PINS = [R, G, B]
 FREQ = 1000
 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PINS, GPIO.OUT, initial=0)
+    
 rp = GPIO.PWM(R, FREQ)
 gp = GPIO.PWM(G, FREQ)
 bp = GPIO.PWM(B, FREQ)
@@ -34,7 +35,10 @@ def init():
 
 @app.route('/gpio/reset')
 def reset():
-    GPIO.cleanup()
+    rp.ChangeDutyCycle(0)
+    gp.ChangeDutyCycle(0)
+    bp.ChangeDutyCycle(0)
+    
     return 'GPIO Reset!'    
 
 @app.route('/gpio/<int:r>/<int:g>/<int:b>')
--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     startup();
 })
 
-var colorWell;
+var colorWell, onBtn, offBtn;
+
+var BASE_URL = 'http://192.168.0.106:5000';
 
 function hexToRGBArray(hex) {
     r = parseInt(hex.substring(1, 3), 16);
@@ -14,7 +16,37 @@ function hexToRGBArray(hex) {
 
 function startup() {
     colorWell = document.querySelector('#color-picker');
+    onBtn = document.querySelector('#on-button');
+    offBtn = document.querySelector('#off-button');
+    
     colorWell.addEventListener('input', updateRGB, false);
+    onBtn.addEventListener('click', rgbOn, false);
+    offBtn.addEventListener('click', rgbOff, false);
+}
+
+function rgbOn() {
+    console.log('on');
+    var url = `${BASE_URL}/gpio`;
+
+    $.get(url)
+        .done(function() {
+            console.log('on')
+        })
+        .fail(function() {
+            console.log('err init gpio')
+        });
+}
+
+function rgbOff() {
+    var url = `${BASE_URL}/gpio/reset`;
+
+    $.get(url)
+        .done(function() {
+            console.log('off');
+        })
+        .fail(function() {
+            console.log('err off gpio')
+        });
 }
 
 function updateRGB(event) {
